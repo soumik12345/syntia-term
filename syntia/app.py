403ace0ac3bbd829c47b6ea423d9af84f9e3f138
soumik12345 <19soumik.rakshit96@gmail.com>
2025-09-03 01:01:46 +0530

    add: basic strcture of syntia -- a text editor

---
+++ syntia/app.py
@@ -0,0 +1,43 @@
+from os import PathLike
+from typing import Union
+
+from textual.app import App
+from textual.containers import Horizontal
+from textual.widgets import DirectoryTree, TextArea
+
+
+class Syntia(App):
+    BINDINGS = [
+        ("ctrl+s", "save_file", "Save file"),
+        ("ctrl+q", "quit", "Quit"),
+    ]
+
+    def __init__(self, root_directory: PathLike, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.root_directory = root_directory
+        self.current_open_file: Union[PathLike, None] = None
+
+    def compose(self):
+        yield Horizontal(
+            DirectoryTree(path=self.root_directory),
+            TextArea(id="editor"),
+        )
+
+    def _on_directory_tree_file_selected(self, event: DirectoryTree.FileSelected):
+        if not event.path.is_file():
+            return
+        self.current_open_file = event.path
+        text_editor: TextArea = self.query_one("#editor", TextArea)
+        text_editor.text = event.path.read_text()
+        if event.path.suffix == ".py":
+            text_editor.syntax = "python"
+        elif event.path.suffix == ".md":
+            text_editor.syntax = "markdown"
+        else:
+            text_editor.syntax = "text"
+
+    def action_save_file(self):
+        if self.current_open_file is None:
+            return
+        editor: TextArea = self.query_one("#editor", TextArea)
+        self.current_open_file.write_text(editor.text)
---
+++ syntia/run.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+
+from syntia.app import Syntia
+
+
+def run_syntia():
+    root_directory = Path(sys.argv[1])
+    if not root_directory.exists():
+        raise FileNotFoundError(f"Root directory {root_directory} does not exist")
+    elif not root_directory.is_dir():
+        root_directory = root_directory.parent
+
+    app = Syntia(root_directory=root_directory)
+    app.run()
