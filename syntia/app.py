11ab96303cd4e301a81d3cbb94a12e9a644478ab
soumik12345 <19soumik.rakshit96@gmail.com>
2025-09-03 01:19:18 +0530

    fix: syntax highlighting

--- syntia/app.py
+++ syntia/app.py
@@ -31,11 +31,11 @@ class Syntia(App):
         text_editor: TextArea = self.query_one("#editor", TextArea)
         text_editor.text = event.path.read_text()
         if event.path.suffix == ".py":
-            text_editor.syntax = "python"
+            text_editor.language = "python"
         elif event.path.suffix == ".md":
-            text_editor.syntax = "markdown"
+            text_editor.language = "markdown"
         else:
-            text_editor.syntax = "text"
+            text_editor.language = "text"
 
     def action_save_file(self):
         if self.current_open_file is None:
