f04f070e1aebb8a00df10e6b894a75c2581951a6
soumik12345 <19soumik.rakshit96@gmail.com>
2025-09-04 16:22:23 +0530

    fix: terminal support

--- syntia/components/__init__.py
+++ syntia/components/__init__.py
@@ -1,5 +1,6 @@
 from syntia.components.horizontal_splitter import HorizontalSplitter
 from syntia.components.tabbed_text_area import TabbedTextArea
 from syntia.components.vertical_splitter import VerticalSplitter
+from syntia.components.terminal import Terminal
 
-__all__ = ["HorizontalSplitter", "VerticalSplitter", "TabbedTextArea"]
+__all__ = ["HorizontalSplitter", "VerticalSplitter", "TabbedTextArea", "Terminal"]
